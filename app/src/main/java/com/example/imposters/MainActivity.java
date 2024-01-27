7ee49b1974d9044797e208f48a3ab3c996a0e2b4
Ruben Taylor <rubentaylor@Rubens-MacBook-Pro-3.local>
2024-01-27 16:00:20 -0500

    Added Battery Manager

--- app/src/main/java/com/example/imposters/MainActivity.java
+++ app/src/main/java/com/example/imposters/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.imposters;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.os.BatteryManager;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView text = (TextView)findViewById(R.id.text1);
+        boolean isCharging = isBatteryCharging(getApplicationContext());
+        String chargingMsg = isCharging ? "Your device is charging" : "Your device is NOT charging";
+        text.setText(chargingMsg);
     }
+
+    public static boolean isBatteryCharging(Context context) {
+        IntentFilter batteryIntent = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
+        Intent batteryStatus = context.registerReceiver(null, batteryIntent);
+        int status = batteryStatus.getIntExtra(BatteryManager.EXTRA_STATUS, -1);
+        boolean charging = status == BatteryManager.BATTERY_STATUS_CHARGING ||
+                status == BatteryManager.BATTERY_STATUS_FULL;
+        return charging;
+    }
+
+
 }
