6fc6ccbf373e8ec9fce5f4a99f1cbc3100e55117
bakp22 <101152896+bakp22@users.noreply.github.com>
2024-01-27 16:29:52 -0500

    Added percentageView

--- app/src/main/java/com/example/imposters/MainActivity.java
+++ app/src/main/java/com/example/imposters/MainActivity.java
@@ -2,7 +2,13 @@ package com.example.imposters;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.BatteryManager;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,40 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Logic to display battery data
+        Context context = getApplicationContext();
+
+        //Code that determines the current charging status
+        IntentFilter ifilter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
+        Intent batteryStatus = context.registerReceiver(null, ifilter);
+
+        // Are we charging / charged?
+        int status = batteryStatus.getIntExtra(BatteryManager.EXTRA_STATUS, -1);
+        boolean isCharging = status == BatteryManager.BATTERY_STATUS_CHARGING ||
+                     status == BatteryManager.BATTERY_STATUS_FULL;
+
+        // How are we charging?
+        int chargePlug = batteryStatus.getIntExtra(BatteryManager.EXTRA_PLUGGED, -1);
+        boolean usbCharge = chargePlug == BatteryManager.BATTERY_PLUGGED_USB;
+        boolean acCharge = chargePlug == BatteryManager.BATTERY_PLUGGED_AC;
+
+        //Determine the current battery level?
+        int level = batteryStatus.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);
+        int scale = batteryStatus.getIntExtra(BatteryManager.EXTRA_SCALE, -1);
+
+        float batteryPct = level * 100 / (float)scale;
+
+        //Display for text
+        TextView percentageView = (TextView) findViewById(R.id.percentage);
+
+        percentageView.setText(Float.toString(batteryPct));
+
+        //Update text whenever battery changes
+
+
+
+    }
+
     }
 }
