1693b1a58cba94b9103a2c2c03171c9d91b782db
bakp22 <101152896+bakp22@users.noreply.github.com>
2024-01-27 20:13:13 -0500

    Refresh Button

--- app/src/main/java/com/example/imposters/MainActivity.java
+++ app/src/main/java/com/example/imposters/MainActivity.java
@@ -16,7 +16,10 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
 import android.provider.Settings;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        refreshPercent();
 
         //Logic to display battery data
         Context context = getApplicationContext();
@@ -35,7 +39,7 @@ public class MainActivity extends AppCompatActivity {
         // Are we charging / charged?
         int status = batteryStatus.getIntExtra(BatteryManager.EXTRA_STATUS, -1);
         boolean isCharging = status == BatteryManager.BATTERY_STATUS_CHARGING ||
-                     status == BatteryManager.BATTERY_STATUS_FULL;
+                status == BatteryManager.BATTERY_STATUS_FULL;
 
         // How are we charging?
         int chargePlug = batteryStatus.getIntExtra(BatteryManager.EXTRA_PLUGGED, -1);
@@ -46,7 +50,7 @@ public class MainActivity extends AppCompatActivity {
         int level = batteryStatus.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);
         int scale = batteryStatus.getIntExtra(BatteryManager.EXTRA_SCALE, -1);
 
-        float batteryPct = level * 100 / (float)scale;
+        float batteryPct = level * 100 / (float) scale;
 
         //Display for text
         TextView percentageView = (TextView) findViewById(R.id.percentage);
@@ -56,11 +60,11 @@ public class MainActivity extends AppCompatActivity {
         //Update text whenever battery changes
 
 
-        TextView text = (TextView)findViewById(R.id.percentage);
+        TextView text = (TextView) findViewById(R.id.percentage);
         boolean isChargingStatus = isBatteryCharging(getApplicationContext());
         String chargingMsg = isChargingStatus ? "Your device is charging" : "Your device is NOT charging";
         text.setText(chargingMsg);
-        if(!checkPackagePermissions())
+        if (!checkPackagePermissions())
             requestPermissions();
 
     }
@@ -73,19 +77,39 @@ public class MainActivity extends AppCompatActivity {
                 status == BatteryManager.BATTERY_STATUS_FULL;
         return charging;
     }
-    public boolean checkPackagePermissions(){
-        AppOpsManager appOps =  (AppOpsManager)
+
+    public boolean checkPackagePermissions() {
+        AppOpsManager appOps = (AppOpsManager)
                 getSystemService(Context.APP_OPS_SERVICE);
         int mode = appOps.checkOpNoThrow(AppOpsManager.OPSTR_GET_USAGE_STATS,
                 android.os.Process.myUid(), getPackageName());
         if (mode == AppOpsManager.MODE_ALLOWED)
-                return true;
+            return true;
         return false;
     }
 
     //https://medium.com/@quiro91/show-app-usage-with-usagestatsmanager-d47294537dab
-    public void requestPermissions(){
+    public void requestPermissions() {
         startActivity(new Intent(Settings.ACTION_USAGE_ACCESS_SETTINGS));
     }
+
+    //Button to refresh percentage level
+    // -> https://www.geeksforgeeks.org/button-in-kotlin/
+    // -> https://developer.android.com/guide/topics/ui/notifiers/toasts#Basics
+
+
+    public void refreshPercent() {
+        Button button = (Button) findViewById(R.id.refreshButton);
+        if (button != null) {
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View it) {
+                    //display a message or something
+                    Toast.makeText(getApplicationContext(), "Battery Level Refreshed :)", Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+    }
+
 
 }
